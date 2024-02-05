dc53ab9903c4b890e2eb4531b92878875a6b9bb0
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-05 16:31:36 +0900

    feat: 다크모드, 라이트모드 셋업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,38 @@ import { Open_Sans } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { koKR } from "@clerk/localizations";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "버블 메시지",
-  description: "실시간 채팅 앱 버블 메시지",
+	title: "버블 메시지",
+	description: "실시간 채팅 앱 버블 메시지",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ko">
-      <body className={font.className}>
-        <ClerkProvider localization={koKR}>
-          {children}
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+	return (
+		<ClerkProvider localization={koKR}>
+			<html
+				lang="ko"
+				suppressHydrationWarning
+			>
+				<body className={cn(font.className, "bg-white, dark:bg-[#313338]")}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						enableSystem={false}
+						storageKey="bubble-theme"
+					>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
